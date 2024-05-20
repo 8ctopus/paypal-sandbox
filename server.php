2bf48192c58e912ebe9684a05e64abb5c3f33ea3
8ctopus <hello@octopuslabs.io>
2024-05-20 09:52:56 +0400

    Add server base

---
+++ server.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Noodlehaus\Config;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$file = __DIR__ . '/.env.php';
+
+if (!file_exists($file)) {
+    echo <<<'TXT'
+    Please create env.php based on env.php.example
+
+    TXT;
+}
+
+$config = Config::load($file);
+
+$loader = new FilesystemLoader(__DIR__ . '/views');
+
+$environment = new Environment($loader, [
+    'auto_reload' => true,
+    'cache' => sys_get_temp_dir(),
+    'debug' => false,
+    'strict_variables' => true,
+]);
+
+echo $environment->render('Store.twig', []);
