67726fead703aafa14e2ac0fa669d3c84f0f7b3c
8ctopus <hello@octopuslabs.io>
2024-05-21 09:50:12 +0400

    Cleanup code with php cs fixer

--- public/router.php
+++ public/router.php
@@ -19,22 +19,22 @@ $serverRequest = ServerRequestCreator::createFromGlobals($_SERVER, $_FILES, $_CO
 
 $router = new NanoRouter(Response::class, ServerRequestFactory::class, true, true);
 
-$router->addRoute(new Route(RouteType::Exact, 'GET', '/', function (ServerRequestInterface $request) : ResponseInterface {
+$router->addRoute(new Route(RouteType::Exact, 'GET', '/', static function (ServerRequestInterface $request) : ResponseInterface {
     return (new Store($request))
         ->showStore();
 }));
 
-$router->addRoute(new Route(RouteType::Exact, 'GET', '/success/', function (ServerRequestInterface $request) : ResponseInterface {
+$router->addRoute(new Route(RouteType::Exact, 'GET', '/success/', static function (ServerRequestInterface $request) : ResponseInterface {
     return (new Store($request))
         ->capturePayment();
 }));
 
-$router->addRoute(new Route(RouteType::Exact, 'GET', '/cancel/', function (ServerRequestInterface $request) : ResponseInterface {
+$router->addRoute(new Route(RouteType::Exact, 'GET', '/cancel/', static function (ServerRequestInterface $request) : ResponseInterface {
     return (new Store($request))
         ->showCancel();
 }));
 
-$router->addRoute(new Route(RouteType::Exact, 'POST', '/create-order/', function (ServerRequestInterface $request) : ResponseInterface {
+$router->addRoute(new Route(RouteType::Exact, 'POST', '/create-order/', static function (ServerRequestInterface $request) : ResponseInterface {
     return (new Store($request))
         ->createOrder();
 }));
